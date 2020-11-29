f8c1bba3c0439aeea6002ae1b68eb6498926befc
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-30 00:14:56 +0200

    add pitch class

--- app/models.py
+++ app/models.py
@@ -1,29 +1,47 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure  = db.Column(db.String(255))
-
-        @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
-
-        @password.setter
-        def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
 
 
-        def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+    pitch = db.relationship('Pitch',backref='user',lazy='dynamic')
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
 
 
-        def __repr__(self):
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+
+    def __repr__(self):
         return f'User {self.username}'
 
 class Role(db.Model):
@@ -32,6 +50,27 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+class Pitch(db.Model):
+    
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    
+    title = db.Column(db.String(255),nullable = False)
+    pitch = db.Column(db.Text(),nullable = False)
+    category = db.Column(db.String(255),index = True,nullable = False)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
 
 
     def __repr__(self):
