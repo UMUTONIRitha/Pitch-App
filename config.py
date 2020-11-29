def8a6cd244ca4f1ed6036e27c726867e07589ba
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-30 00:07:18 +0200

    add class config

--- config.py
+++ config.py
@@ -5,10 +5,21 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/pitch_app'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 class ProdConfig(Config):
+
     pass
 class DevConfig(Config):
+
     DEBUG = True
+    
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
