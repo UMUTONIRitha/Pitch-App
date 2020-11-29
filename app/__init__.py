f9ba81deb7295cac8d66c09ec8caecaa4210b3b4
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-30 00:14:09 +0200

    add imports

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 login_manager = LoginManager()
@@ -13,6 +14,7 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -25,6 +27,7 @@ def create_app(config_name):
 
     # configure UploadSet
     configure_uploads(app,photos)
+    mail.init_app(app)
     
 
     # Registering the blueprint
