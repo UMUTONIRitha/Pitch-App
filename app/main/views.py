2852b418c9626054751ca295ddd6dac2cc5229b7
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-30 00:13:20 +0200

    add imports and route

---
+++ app/main/views.py
@@ -0,0 +1,70 @@
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User,Pitch
+from . import main
+from flask_login import login_required,current_user
+from .forms import UpdateProfile
+from .. import db,photos
+
+
+
+
+# Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+    
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+
+
+
+
+
+
+
+# @main.route('/pitch/', methods = ['GET','POST'])
+# @login_required
+# def new_review(id):
